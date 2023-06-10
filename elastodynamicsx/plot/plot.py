3191a647c4a7409de00137dd4137e4d24dde4dfd
Pierric Mora <pierric.mora@univ-eiffel.fr>
2023-06-10 23:29:18 +0200

    enable plotting the undeformed mesh using 'ref_mesh=True' (vector fields)

--- elastodynamicsx/plot/plot.py
+++ elastodynamicsx/plot/plot.py
@@ -4,12 +4,14 @@
 #
 # SPDX-License-Identifier: MIT
 
+import time
+
 import numpy as np
 import matplotlib.pyplot as plt
+import pyvista
+
 from dolfinx import plot, fem
 from dolfinx.mesh import Mesh
-import pyvista
-import time
 from petsc4py import PETSc
 
 from typing import Union, List
@@ -34,6 +36,7 @@ def is_notebook() -> bool:
             return False  # Other type (?)
     except NameError:
         return False      # Probably standard Python interpreter
+
 
 if is_notebook():
     # This "ipyvtklink" backend supports almost every pyvista feature. However, it is run on server, leading to great lags
@@ -62,7 +65,7 @@ def plot_mesh(mesh, cell_tags=None, **kwargs):
     Returns:
         The pyvista.Plotter
 
-    Example of use:
+    Example:
 
         from mpi4py import MPI
         from dolfinx.mesh import create_unit_square
@@ -80,9 +83,11 @@ def plot_mesh(mesh, cell_tags=None, **kwargs):
     p = pyvista.Plotter()
     grid = pyvista.UnstructuredGrid(*plot.create_vtk_mesh(mesh, mesh.topology.dim))
     num_local_cells = mesh.topology.index_map(mesh.topology.dim).size_local
+
     if not cell_tags is None:
         grid.cell_data["Marker"] = cell_tags.values[cell_tags.indices<num_local_cells]
         grid.set_active_scalars("Marker")
+
     actor = p.add_mesh(grid, show_edges=True)
     #
     if mesh.topology.dim==2:
@@ -96,23 +101,23 @@ def live_plotter(u:'fem.Function', refresh_step:int=1, **kwargs) -> pyvista.Plot
     return plotter(u, **kwargs)
 
 
-def plotter(*all_u:Union[List['fem.Function'], Mesh], **kwargs) -> pyvista.Plotter:
+def plotter(*args:Union[List['fem.Function'], Mesh], **kwargs) -> pyvista.Plotter:
     """
     A generic function to plot a mesh or one/several fields
     """
-    u1 = all_u[0]
+    u1 = args[0]
 
     if isinstance(u1, Mesh):
-        return plot_mesh(*all_u, **kwargs)
+        return plot_mesh(*args, **kwargs)
 
     else:
         # test whether u is scalar or vector and returns the appropriate plotter
         nbcomps = u1.function_space.element.num_sub_elements  # number of components if vector space, 0 if scalar space
 
         if nbcomps == 0:
-            return CustomScalarPlotter(*all_u, **kwargs)
+            return CustomScalarPlotter(*args, **kwargs)
         else:
-            return CustomVectorPlotter(*all_u, **kwargs)
+            return CustomVectorPlotter(*args, **kwargs)
 
 
 ### -------------------------------------- ###
@@ -141,7 +146,8 @@ class CustomScalarPlotter(pyvista.Plotter):
             self._trans = np.angle
 
         for u_ in all_scalars:
-            if u_ is None: break
+            if u_ is None:
+                break
             topology, cell_types, geom = plot.create_vtk_mesh(u_.function_space)
             grid = pyvista.UnstructuredGrid(topology, cell_types, geom)
             grid.point_data["u"] = self._trans(u_.x.array)
@@ -219,14 +225,14 @@ class CustomScalarPlotter(pyvista.Plotter):
 class CustomVectorPlotter(pyvista.Plotter):
 
     default_cmap = plt.cm.get_cmap("viridis")
-    
+
     def __init__(self, *all_vectors, **kwargs):
         ###
         self.grids=[]
         self._refresh_step = kwargs.pop('refresh_step', 1)
         self._tsleep = kwargs.pop('sleep', 0.01)
         dims = []
-        
+
         self._trans = lambda x:x
         cmplx = kwargs.pop('complex', 'real')
         if   cmplx.lower() == 'real':
@@ -237,14 +243,14 @@ class CustomVectorPlotter(pyvista.Plotter):
             self._trans = np.abs
         elif cmplx.lower() == 'angle':
             self._trans = np.angle
-        
+
         if 'warp_factor' in kwargs:
             self.warp_factor = kwargs.pop('warp_factor')
         elif 'clim' in kwargs and np.amax(np.abs(kwargs['clim']))>0:
             self.warp_factor = 0.5/np.amax(np.abs(kwargs['clim']))
         else:
             self.warp_factor = 1
-        
+
         for u_ in all_vectors:
             if u_ is None: break
             topology, cell_types, geom = plot.create_vtk_mesh(u_.function_space)
@@ -254,7 +260,7 @@ class CustomVectorPlotter(pyvista.Plotter):
             grid.point_data["u_nrm"] = np.linalg.norm(u3D, axis=1)
             self.grids.append(grid)
             dims.append(u_.function_space.mesh.topology.dim)
-        
+
         #nbcomps = max(1, u_.function_space.element.num_sub_elements)
         if len(self.grids)==1:
             defaultShape = (1,1)
@@ -263,13 +269,13 @@ class CustomVectorPlotter(pyvista.Plotter):
             defaultShape = '1|'+str(len(self.grids)-1)
         shape = kwargs.pop('shape', defaultShape)
         super().__init__(shape=shape)
-        
+
         show_edges = kwargs.pop('show_edges', 'first')
         if   show_edges == False:
             show_edges = 'none'
         elif show_edges == True:
             show_edges = 'all'
-        
+
         if   show_edges == 'none':
             show_edges = [False for i in range(len(self.grids))]
         elif show_edges == 'all':
@@ -277,11 +283,18 @@ class CustomVectorPlotter(pyvista.Plotter):
         elif show_edges == 'first':
             show_edges = [i==0  for i in range(len(self.grids))]
 
+        show_ref_mesh = kwargs.pop('ref_mesh', False)
+        if show_ref_mesh and not 'opacity' in kwargs.keys():
+            kwargs['opacity'] = 0.8
+
         labels = kwargs.pop('labels', ['' for i in range(len(self.grids))])
         cmap = kwargs.pop('cmap', CustomVectorPlotter.default_cmap)
         sargs = dict(color="black", position_y=0.0)
         for i,grid in enumerate(self.grids):
-            if len(self.grids)>1: self.subplot(i)
+            if len(self.grids)>1:
+                self.subplot(i)
+            if show_ref_mesh:
+                self.add_mesh(grid, style='wireframe', color='black')
             self.add_text(labels[i])
             warped = grid.warp_by_vector("u", factor=self.warp_factor)
             grid.warped = warped
@@ -290,12 +303,14 @@ class CustomVectorPlotter(pyvista.Plotter):
                 self.view_xy()
                 self.camera.zoom(1.2)
 
-        if len(self.grids)>1: self.subplot(0) #resets the focus to first subplot
+        if len(self.grids)>1:
+            self.subplot(0)  # resets the focus to first subplot
+
 
     def update_vectors(self, *all_vectors, render=True):
         """
         Calls pyvista.Plotter.update_coordinates and .update_scalars for all subplots
-        
+
         Args:
             all_vectors: tuple of np.ndarray
                 e.g. all_vectors = (u1.x, u2.x, ...) where u1, u2 are dolfinx.fem.Function
@@ -309,6 +324,7 @@ class CustomVectorPlotter(pyvista.Plotter):
             super().update_scalars(np.linalg.norm(u3D, axis=1), mesh=grid.warped, render=False)
         if render:
             self.render()
+
 
     def live_plotter_update_function(self, i:int, vec:PETSc.Vec) -> None:
         if not(isinstance(vec, PETSc.Vec)):
@@ -324,6 +340,7 @@ class CustomVectorPlotter(pyvista.Plotter):
                 self.update_vectors(loc_v.array)
             time.sleep(self._tsleep)
 
+
     def add_time_browser(self, update_fields_function:'function', timesteps:np.ndarray, **kwargs_slider):
         self._time_browser_cbck  = update_fields_function
         self._time_browser_times = timesteps
@@ -332,18 +349,18 @@ class CustomVectorPlotter(pyvista.Plotter):
             updated_fields = self._time_browser_cbck(i)
             self.update_vectors(*updated_fields)
         self.add_slider_widget(updateTStep, [timesteps[0], timesteps[-1]], **kwargs_slider)
-        
+
 
 
 def spy_petscMatrix(Z:PETSc.Mat, *args, **kwargs) -> 'matplotlib.pyplot.spy':
     """
     matplotlib.pyplot.spy with Z being a petsc4py.PETSc.Mat object
-    
+
     Args:
         Z:      The array to be plotted, of type petsc4py.PETSc.Mat
         args:   Passed to matplotlib.pyplot.spy (see doc)
         kwargs: Passed to matplotlib.pyplot.spy (see doc)
-    
+
     Returns:
         See doc of matplotlib.pyplot.spy
     """
@@ -353,6 +370,7 @@ def spy_petscMatrix(Z:PETSc.Mat, *args, **kwargs) -> 'matplotlib.pyplot.spy':
     # ai, aj, av    = Z.getValuesCSR()
     # Z_scipysparse = scipy.sparse.csr_matrix((av, aj, ai))
     return plt.spy(scipy.sparse.csr_matrix(Z.getValuesCSR()[::-1]), *args, **kwargs)
+
 
 ### ------------------------------------ ###
 ### --- define useful util functions --- ###
@@ -369,6 +387,7 @@ def get_3D_array_from_FEFunction(u_):
     else:
         return u_.x.array.reshape((nbpts, 3))
 
+
 def get_3D_array_from_nparray(u_, nbpts):
     """Not intended to be called by user"""
     # u_ is a np.array
@@ -378,4 +397,3 @@ def get_3D_array_from_nparray(u_, nbpts):
         return np.append(u_.reshape((nbpts, nbcomps)), z0s, axis=1)
     else:
         return u_.reshape((nbpts, 3))
-
