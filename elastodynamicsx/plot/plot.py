e2915aa0d047fbfda70cda3e40a14b84048d7f61
Pierric Mora <pierric.mora@univ-eiffel.fr>
2026-01-20 10:13:36 +0100

    fix stop record scalar

--- elastodynamicsx/plot/plot.py
+++ elastodynamicsx/plot/plot.py
@@ -233,14 +233,15 @@ class CustomScalarPlotter(pyvista.Plotter):
     def live_plotter_stop(self):
         is_recording = hasattr(self, 'mwriter') and self.mwriter is not None
         if is_recording:
-            self.close()
             fname = self.mwriter.request.filename
+            extension = self.mwriter.request.extension
+            self.close()
             try:
                 import IPython.display
-                if self.mwriter.request.extension.lower() == '.gif':
+                if extension.lower() == '.gif':
                     im = IPython.display.Image(open(fname, 'rb').read())
                     IPython.display.display(im)  # display .gif in notebook
-                elif self.mwriter.request.extension.lower() == '.mp4':
+                elif extension.lower() == '.mp4':
                     vi = IPython.display.Video(fname)
                     if _DOCS_CFG:
                         vi.embed = True
@@ -433,14 +434,14 @@ class CustomVectorPlotter(pyvista.Plotter):
         is_recording = hasattr(self, 'mwriter') and self.mwriter is not None
         if is_recording:
             fname = self.mwriter.request.filename
-            extention = self.mwriter.request.extension
+            extension = self.mwriter.request.extension
             self.close()
             try:
                 import IPython.display
-                if extention.lower() == '.gif':
+                if extension.lower() == '.gif':
                     im = IPython.display.Image(open(fname, 'rb').read())
                     IPython.display.display(im)  # display .gif in notebook
-                elif extention.lower() == '.mp4':
+                elif extension.lower() == '.mp4':
                     vi = IPython.display.Video(fname)
                     if _DOCS_CFG:
                         vi.embed = True
